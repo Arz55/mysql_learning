705ee3ddbe0d8db0d04dc8725249fadb0c5d3f69
Arz55 <138655474+Arz55@users.noreply.github.com>
2025-05-21 19:24:29 +0800

    sql_ex_and_quiz_2

---
+++ ex12.sql
@@ -0,0 +1,4 @@
+SELECT *
+FROM customers
+-- LIMIT 3  -- 显示最前面的3条信息
+LIMIT 6,3  -- 挑过前六条，显示剩下的最前面3条消息
---
+++ quiz10.sql
@@ -0,0 +1,6 @@
+-- Get the top 3 loyal customers
+
+SELECT *
+FROM customers
+ORDER BY points DESC
+LIMIT 3
